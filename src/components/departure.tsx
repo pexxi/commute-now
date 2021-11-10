501e5af1c66e2ab5073e7933b1d2e537a49ee754
Pekka Kokkonen <hello@pexxi.dev>
2021-11-10 16:15:21 +0200

    Update departure layout

--- src/components/departure.tsx
+++ src/components/departure.tsx
@@ -11,41 +11,47 @@ interface Props {
 }
 function Departure({ train, departure, arrival }: Props) {
   return (
-    <Card style={{ minWidth: "25rem" }}>
-      <Grid container direction="row">
-        <Box bgcolor="secondary.main" p={2}>
-          <Grid item justifyContent="center" alignItems="center">
-            <Typography variant="h3" style={{ color: "white" }} gutterBottom={false}>
-              {train.commuterLineID}
-            </Typography>
-          </Grid>
-        </Box>
-        <Grid item container direction="row" xs justifyContent="space-between">
-          <Grid item>
-            <Box p={2}>
-              <Typography>
-                {format(new Date(departure.scheduledTime), "H:mm", {
-                  locale: fi,
-                })}
+    <Grid container direction="column" py={1}>
+      <Card>
+        <Grid item container direction="row">
+          <Box bgcolor="secondary.main" p={2} sx={{ minWidth: "4rem" }}>
+            <Grid item justifyContent="center" alignItems="center">
+              <Typography variant="h3" style={{ color: "white" }} gutterBottom={false}>
+                {train.commuterLineID}
               </Typography>
-              <Typography>Track: {departure.commercialTrack}</Typography>
-            </Box>
-          </Grid>
-          <Grid item container justifyContent="center" alignItems="center" xs>
-            <ArrowForwardIcon />
-          </Grid>
-          <Grid item>
-            <Box p={2}>
-              <Typography>
-                {format(new Date(arrival.scheduledTime), "H:mm", {
-                  locale: fi,
-                })}
-              </Typography>
-            </Box>
+            </Grid>
+          </Box>
+          <Grid item container direction="row" xs justifyContent="space-between">
+            <Grid item>
+              <Box p={2}>
+                <Typography>
+                  {format(new Date(departure.scheduledTime), "H:mm", {
+                    locale: fi,
+                  })}
+                </Typography>
+                {train.cancelled ? (
+                  <Typography color="red">Cancelled</Typography>
+                ) : (
+                  <Typography>Track: {departure.commercialTrack}</Typography>
+                )}
+              </Box>
+            </Grid>
+            <Grid item container justifyContent="center" alignItems="center" xs>
+              <ArrowForwardIcon />
+            </Grid>
+            <Grid item>
+              <Box p={2}>
+                <Typography>
+                  {format(new Date(arrival.scheduledTime), "H:mm", {
+                    locale: fi,
+                  })}
+                </Typography>
+              </Box>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Card>
+      </Card>
+    </Grid>
   );
 }
 
